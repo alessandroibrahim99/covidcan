0eccbfaa5873e109222b81088650d018d8a6ddec
alessandroibrahim99 <38700012+alessandroibrahim99@users.noreply.github.com>
2020-10-23 12:39:02 -0400

    Update data.py

--- python/data.py
+++ python/data.py
@@ -1,16 +1,16 @@
 
 import pandas as pd
 
-#df_cases1 = pd.read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv", sep=",")
-#df_deaths1 = pd.read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv", sep=",")
-#df_cases = df_cases1.loc[df_cases1['Country/Region']== 'Canada']
-#df_deaths = df_deaths1.loc[df_deaths1['Country/Region']== 'Canada']
+df_cases1 = pd.read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv", sep=",")
+df_deaths1 = pd.read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv", sep=",")
+dtf_cases = df_cases1.loc[df_cases1['Country/Region']== 'Canada']
+dtf_deaths = df_deaths1.loc[df_deaths1['Country/Region']== 'Canada']
 
 class Data():
     
     def get_data(self):
-        self.dtf_cases = pd.read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv", sep=",")
-        self.dtf_deaths = pd.read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv", sep=",")
+        #self.dtf_cases = pd.read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv", sep=",")
+        #self.dtf_deaths = pd.read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv", sep=",")
         
         self.countrylist = ["World"] + self.dtf_cases["Country/Region"].unique().tolist()
         #self.countrylist = ["World"] + self.dtf_cases["Province/State"].unique().tolist()
