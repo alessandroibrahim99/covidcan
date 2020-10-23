ac386826a7bbc06621f2a93f8cb1184ab0af3858
alessandroibrahim99 <38700012+alessandroibrahim99@users.noreply.github.com>
2020-10-23 12:24:15 -0400

    Update data.py

--- python/data.py
+++ python/data.py
@@ -20,7 +20,8 @@ class Data():
     def group_by_country(dtf, country):
         #dtf = dtf.drop(['Province/State','Lat','Long'], axis=1).groupby("Country/Region").sum().T
         dtf = dtf.drop(['Country/Region','Lat','Long'], axis=1).groupby("Province/State").sum().T
-        dtf["World"] = dtf.sum(axis=1)
+        #dtf["World"] = dtf.sum(axis=1)
+        dtf["Canada"] = dtf.sum(axis=1)
         dtf = dtf[country]
         dtf.index = pd.to_datetime(dtf.index, infer_datetime_format=True)
         ts = pd.DataFrame(index=dtf.index, data=dtf.values, columns=["data"])
