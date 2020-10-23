b021d6ebe94e8a40b02c246dd63495f68fe296bd
alessandroibrahim99 <38700012+alessandroibrahim99@users.noreply.github.com>
2020-10-23 12:22:22 -0400

    Update data.py

--- python/data.py
+++ python/data.py
@@ -1,26 +1,30 @@
 
 import pandas as pd
 
-
+df_cases1 = pd.read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv", sep=",")
+df_deaths1 = pd.read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv", sep=",")
+df_cases = df_cases1.loc[df_cases1['Country/Region']== 'Canada']
+df_deaths = df_deaths1.loc[df_deaths1['Country/Region']== 'Canada']
 
 class Data():
     
     def get_data(self):
-        self.dtf_cases = pd.read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv", sep=",")
-        self.dtf_deaths = pd.read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv", sep=",")
-        #self.geo = self.dtf_cases[['Country/Region','Lat','Long']].drop_duplicates("Country/Region", keep='first')
-        self.countrylist = ["World"] + self.dtf_cases["Country/Region"].unique().tolist()
+        #self.dtf_cases = pd.read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv", sep=",")
+        #self.dtf_deaths = pd.read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv", sep=",")
+        
+        #self.countrylist = ["World"] + self.dtf_cases["Country/Region"].unique().tolist()
+        self.countrylist = ["Canada"] + self.dtf_cases["Province/State"].unique().tolist()
 
     
     @staticmethod
     def group_by_country(dtf, country):
-        dtf = dtf.drop(['Province/State','Lat','Long'], axis=1).groupby("Country/Region").sum().T
+        #dtf = dtf.drop(['Province/State','Lat','Long'], axis=1).groupby("Country/Region").sum().T
+        dtf = dtf.drop(['Country/Region','Lat','Long'], axis=1).groupby("Province/State").sum().T
         dtf["World"] = dtf.sum(axis=1)
         dtf = dtf[country]
         dtf.index = pd.to_datetime(dtf.index, infer_datetime_format=True)
         ts = pd.DataFrame(index=dtf.index, data=dtf.values, columns=["data"])
         return ts
-    
     
     @staticmethod
     def calculate_mortality(ts_deaths, ts_cases):
